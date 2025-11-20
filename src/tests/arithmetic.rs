a9ea6f6c4a3ea71cff34cdbecf63e4296a2d0374
Hanno Braun <hello@hannobraun.com>
2025-11-20 19:36:06 +0100

    Add test for wrapping add on unsigned overflow
    This doesn't require any changes, due to the way values are represented.
    I still want to have this test though, as those implementation details
    might change.

--- src/tests/arithmetic.rs
+++ src/tests/arithmetic.rs
@@ -10,3 +10,14 @@ fn add() {
     assert_eq!(eval.effect, None);
     assert_eq!(eval.stack, vec![3]);
 }
+
+#[test]
+fn add_wraps_on_signed_overflow() {
+    // An addition that overflows the range of a signed 32-bit integer wraps.
+
+    let mut eval = Eval::start("2147483647 1 +");
+    eval.run();
+
+    assert_eq!(eval.effect, None);
+    assert_eq!(eval.stack, vec![2147483648]);
+}
