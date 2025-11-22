069f0a93248f49c1d3fbb9ca519fd6aecb2ee5c7
Hanno Braun <hello@hannobraun.com>
2025-11-22 15:38:50 +0100

    Add test for empty script
    This new test explicitly covers a behavior that was previously only
    implicitly covered by other tests.

--- src/tests/evaluation.rs
+++ src/tests/evaluation.rs
@@ -1,6 +1,17 @@
 use crate::{Effect, Eval};
 
 #[test]
+fn empty_script_does_nothing() {
+    // Running an empty script does nothing.
+
+    let mut eval = Eval::start("");
+    eval.run();
+
+    assert_eq!(eval.effect, None);
+    assert_eq!(eval.stack.values, vec![]);
+}
+
+#[test]
 fn starting_evaluation_does_not_evaluate_any_operators() {
     // Starting the evaluation readies it, but does not yet evaluate any
     // operators.
