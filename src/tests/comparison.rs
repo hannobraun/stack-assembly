9b1c91d7bbf919b316e291768b79107fb1c0bff8
Hanno Braun <hello@hannobraun.com>
2025-11-27 19:56:27 +0100

    Add `>` operator

--- src/tests/comparison.rs
+++ src/tests/comparison.rs
@@ -91,3 +91,38 @@ fn equals_outputs_zero_if_not_equal() {
     assert_eq!(eval.effect, Some(Effect::OutOfTokens));
     assert_eq!(eval.stack.to_u32_slice(), &[0]);
 }
+
+#[test]
+fn larger_outputs_zero_if_smaller() {
+    // The `>` operator output `0`, if its first input is smaller than the
+    // second, treating both inputs as signed.
+
+    let mut eval = Eval::start("-1 0 >");
+    eval.run();
+
+    assert_eq!(eval.effect, Some(Effect::OutOfTokens));
+    assert_eq!(eval.stack.to_u32_slice(), &[0]);
+}
+
+#[test]
+fn larger_outputs_zero_if_equal() {
+    // The `>` operator output `0`, if its two inputs are equal.
+
+    let mut eval = Eval::start("0 0 >");
+    eval.run();
+
+    assert_eq!(eval.effect, Some(Effect::OutOfTokens));
+    assert_eq!(eval.stack.to_u32_slice(), &[0]);
+}
+
+#[test]
+fn larger_outputs_one_if_larger() {
+    // The `>` operator output `1`, if its first outputs is larger then the
+    // second, treating both inputs as signed.
+
+    let mut eval = Eval::start("0 -1 >");
+    eval.run();
+
+    assert_eq!(eval.effect, Some(Effect::OutOfTokens));
+    assert_eq!(eval.stack.to_u32_slice(), &[1]);
+}
