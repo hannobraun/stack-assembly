d82ce8309965d2af7741f29dc0d8ab85bd33df6e
Hanno Braun <hello@hannobraun.com>
2025-12-03 18:29:57 +0100

    Implement `fmt::Debug` for `Memory`

--- src/memory.rs
+++ src/memory.rs
@@ -1,8 +1,32 @@
+use std::fmt;
+
 use crate::Value;
 
 /// # A linear memory, freely addressable per word
-#[derive(Debug)]
 pub struct Memory {
     /// # The values in the memory
     pub values: Vec<Value>,
+}
+
+impl fmt::Debug for Memory {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // This is not perfect, but it's way more compact than the derived
+        // implementation. That was so verbose as to be barely usable.
+
+        let mut values = self.values.iter().peekable();
+
+        write!(f, "[")?;
+
+        while let Some(value) = values.next() {
+            write!(f, "{value:?}")?;
+
+            if values.peek().is_some() {
+                write!(f, ", ")?;
+            }
+        }
+
+        write!(f, "]")?;
+
+        Ok(())
+    }
 }
