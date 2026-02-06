434b5e1cb6d9e500ffe5a2ba8336ebf7e32a06f0
Hanno Braun <hello@hannobraun.com>
2026-02-06 21:03:33 +0100

    Add `Script::compile`

--- crates/stack-assembly/src/eval.rs
+++ crates/stack-assembly/src/eval.rs
@@ -141,51 +141,8 @@ impl Eval {
     /// for evaluation. To evaluate any operators, you must call [`Eval::run`]
     /// or [`Eval::step`].
     pub fn start(script: &str) -> Self {
-        let mut operators = Vec::new();
-        let mut labels = Vec::new();
-
-        for line in script.lines() {
-            for token in line.split_whitespace() {
-                if token.starts_with("#") {
-                    // This is a comment. Ignore the rest of the line.
-                    break;
-                }
-
-                let operator = if let Some((name, "")) = token.rsplit_once(":")
-                {
-                    labels.push(Label {
-                        name: name.to_string(),
-                        operator: operators.len(),
-                    });
-                    continue;
-                } else if let Some(("", name)) = token.split_once("@") {
-                    Operator::Reference {
-                        name: name.to_string(),
-                    }
-                } else if let Some(("", value)) = token.split_once("0x")
-                    && let Ok(value) = i32::from_str_radix(value, 16)
-                {
-                    Operator::Integer { value }
-                } else if let Some(("", value)) = token.split_once("0x")
-                    && let Ok(value) = u32::from_str_radix(value, 16)
-                {
-                    Operator::integer_u32(value)
-                } else if let Ok(value) = token.parse::<i32>() {
-                    Operator::Integer { value }
-                } else if let Ok(value) = token.parse::<u32>() {
-                    Operator::integer_u32(value)
-                } else {
-                    Operator::Identifier {
-                        value: token.to_string(),
-                    }
-                };
-
-                operators.push(operator);
-            }
-        }
-
         Self {
-            script: Script { operators, labels },
+            script: Script::compile(script),
             next_operator: 0,
             call_stack: Vec::new(),
             effect: None,
--- crates/stack-assembly/src/script.rs
+++ crates/stack-assembly/src/script.rs
@@ -4,6 +4,55 @@ pub struct Script {
     pub labels: Vec<Label>,
 }
 
+impl Script {
+    pub fn compile(script: &str) -> Self {
+        let mut operators = Vec::new();
+        let mut labels = Vec::new();
+
+        for line in script.lines() {
+            for token in line.split_whitespace() {
+                if token.starts_with("#") {
+                    // This is a comment. Ignore the rest of the line.
+                    break;
+                }
+
+                let operator = if let Some((name, "")) = token.rsplit_once(":")
+                {
+                    labels.push(Label {
+                        name: name.to_string(),
+                        operator: operators.len(),
+                    });
+                    continue;
+                } else if let Some(("", name)) = token.split_once("@") {
+                    Operator::Reference {
+                        name: name.to_string(),
+                    }
+                } else if let Some(("", value)) = token.split_once("0x")
+                    && let Ok(value) = i32::from_str_radix(value, 16)
+                {
+                    Operator::Integer { value }
+                } else if let Some(("", value)) = token.split_once("0x")
+                    && let Ok(value) = u32::from_str_radix(value, 16)
+                {
+                    Operator::integer_u32(value)
+                } else if let Ok(value) = token.parse::<i32>() {
+                    Operator::Integer { value }
+                } else if let Ok(value) = token.parse::<u32>() {
+                    Operator::integer_u32(value)
+                } else {
+                    Operator::Identifier {
+                        value: token.to_string(),
+                    }
+                };
+
+                operators.push(operator);
+            }
+        }
+
+        Self { operators, labels }
+    }
+}
+
 #[derive(Debug)]
 pub enum Operator {
     Identifier { value: String },
