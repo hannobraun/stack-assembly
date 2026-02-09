37bad1edda8c29d24f63852c2c124772d8e34f33
Hanno Braun <hello@hannobraun.com>
2026-02-09 20:24:22 +0100

    Don't allow modifying effect through `Eval::run`
    The old return type was questionable, as it allowed changing the type of
    effect, which doesn't seem useful, but not resetting it, which is the
    only kind of modification that _does_ seem useful.
    
    Either way, the new return type is simpler, and this change paves the
    way for restricting other useless ways in which the host can interact
    with the effect.

--- crates/stack-assembly/src/eval.rs
+++ crates/stack-assembly/src/eval.rs
@@ -154,7 +154,7 @@ impl Eval {
     ///
     /// [`effect`]: #structfield.effect
     /// [`next_operator`]: #structfield.next_operator
-    pub fn run(&mut self, script: &Script) -> &mut Effect {
+    pub fn run(&mut self, script: &Script) -> Effect {
         while self.effect.is_none() {
             self.step(script);
         }
@@ -162,7 +162,7 @@ impl Eval {
         // It's a bit of a shame we have to unwrap the `Option` like this, but
         // I tried doing it from within the loop, and failed due to the borrow
         // checker.
-        let Some(effect) = &mut self.effect else {
+        let Some(effect) = self.effect else {
             unreachable!(
                 "An effect must have triggered, or we wouldn't have exited the \
                 loop just now."
